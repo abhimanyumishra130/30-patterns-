0674fa922daff1444d5b80ad5d20b3963ba7b92e
Abhimanyu Mishra <83170741+abhimanyumishra130@users.noreply.github.com>
2021-05-08 19:06:42 +0530

    Update 5pattern.java

--- 5pattern.java
+++ 5pattern.java
@@ -3,13 +3,13 @@ public class Main {
     public static void main(String args[]) {
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
-        int a=n;
-         for(int i=1;i<=n;i++){
-             for(int j=a;j>=1;j--){
+        
+         for(int i=n;i>=1;i--){
+             for(int j=1;j<=i;j++){
                  System.out.print("* ");
              }
              System.out.println();
-             a--;
+             
          }
        
     }
